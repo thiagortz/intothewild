e75af42b1a837c22b6698270cb821479f7f8a71d
Thiago Dias <thiago-dias@outlook.com>
2018-11-04 16:16:40 -0200

    add entity note

---
+++ src/main/kotlin/com/surprisebox/intothewild/model/Note.kt
@@ -0,0 +1,12 @@
+package com.surprisebox.intothewild.model
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity
+class Note(@Id
+           @GeneratedValue
+           val id: Long = 0L,
+           val title: String = "",
+           val description: String = "")
